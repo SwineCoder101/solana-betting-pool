aa781c9ec48d308f245141e93d3fa5f6637baad4
Swinecode101 <liamfaruq@gmail.com>
2025-01-05 17:30:46 +1100

    add-competition

---
+++ anchor/programs/horserace/src/lib.rs
@@ -0,0 +1,49 @@
+use anchor_lang::prelude::*;
+
+pub mod states;
+pub mod instructions;
+
+use instructions::*;
+
+declare_id!("YourProgramPubkey11111111111111111111111111");
+
+#[program]
+pub mod horse_race {
+    use super::*;
+
+    pub fn run_create_competition(
+        ctx: Context<CreateCompetition>,
+        token_a: Pubkey,
+        price_feed_id: String,
+        admin: Vec<Pubkey>,
+        house_cut_factor: u8,
+        min_payout_ratio: u8,
+    ) -> Result<()> {
+        instructions::admin::create_competition::run_create_competition(
+            ctx,
+            token_a,
+            price_feed_id,
+            admin,
+            house_cut_factor,
+            min_payout_ratio,
+        )
+    }
+
+    pub fn run_update_competition(
+        ctx: Context<UpdateCompetition>,
+        token_a: Pubkey,
+        price_feed_id: String,
+        admin: Vec<Pubkey>,
+        house_cut_factor: u8,
+        min_payout_ratio: u8,
+    ) -> Result<()> {
+        instructions::admin::update_competition::run_update_competition(
+            ctx,
+            token_a,
+            price_feed_id,
+            admin,
+            house_cut_factor,
+            min_payout_ratio,
+        )
+    }
+}
